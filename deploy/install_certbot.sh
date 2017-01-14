9a1646047d6d6f8626732d1c8fa2edf7ae6f7114
Sergei Basharov <sergey@basharov.net>
2017-01-14 12:22:22 +0200

    letsencrypt config

--- deploy/install_certbot.sh
+++ deploy/install_certbot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 yes | sudo apt-get install letsencrypt
-#letsencrypt certonly --standalone -d domio.in -d www.domio.in -d api.domio.in
+#letsencrypt certonly --standalone -d domio.in -d www.domio.in -m sergei@basharov.net --agree-tos
