aa45272ca77464d4ab82fe482317239f1917de36
Sergei Basharov <sergey@basharov.net>
2017-01-14 13:47:21 +0200

    folders creation fixesπ

--- deploy/copy_templates.sh
+++ deploy/copy_templates.sh
@@ -5,6 +5,8 @@ templates_folder=/usr/local/domio_public/templates
 
 echo Copying templates folder...
 
+mkdir -p /usr/local/domio_public
+
 rm -rf ${templates_folder}
 yes | mv -f ~/domiopublic/src/domio_public/templates ${templates_folder}
 
