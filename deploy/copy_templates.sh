ababc16836a0c960fbdc09820517ce8291d6fbe9
Sergei Basharov <sergey@basharov.net>
2017-01-14 13:42:27 +0200

    templates folder path change

--- deploy/copy_templates.sh
+++ deploy/copy_templates.sh
@@ -6,6 +6,6 @@ templates_folder=/usr/local/domio_public/templates
 echo Copying templates folder...
 
 rm -rf ${templates_folder}
-yes | mv -f ~/domiopublic/src/templates ${templates_folder}
+yes | mv -f ~/domiopublic/src/domio_public/templates ${templates_folder}
 
 echo Templates copied!
