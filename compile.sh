4dbdffa273a53840987b1367e7b47c3696105501
Sergei Basharov <sergey@basharov.net>
2017-01-14 10:41:24 +0200

    templates setup

--- compile.sh
+++ compile.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+PROJECT_NAME=domio_public
+
 #buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S'`
 buildstamp=`date -u '+%s'`
 hash=`git rev-parse --short HEAD`
 version=`git tag -l --points-at HEAD`
+
+
 
 export GOPATH=$PWD
 
@@ -13,16 +17,22 @@ echo "  Buildstamp: ${buildstamp}"
 echo "  Hash:       ${hash}"
 echo "  Version:    ${version}"
 echo ---------------------------
-
 echo
+
+
 echo Compiling for Windows...
 export GOARCH=amd64
 export GOOS=windows
-PROJECT_NAME=domio_public
-go build -o /usr/local/bin/$PROJECT_NAME.exe -ldflags "-X main.Buildstamp=$buildstamp -X main.Hash=$hash  -X main.Version=$version" $PROJECT_NAME
+
+go build -o /usr/local/bin/${PROJECT_NAME}_win.exe -ldflags "-X main.Buildstamp=$buildstamp -X main.Hash=$hash  -X main.Version=$version" ${PROJECT_NAME}
 
 
 echo Compiling for Linux...
 export GOARCH=amd64
 export GOOS=linux
-go build -o /usr/local/bin/$PROJECT_NAME -ldflags "-X main.Buildstamp=$buildstamp -X main.Hash=$hash  -X main.Version=$version" $PROJECT_NAME
+go build -o /usr/local/bin/${PROJECT_NAME}_linux -ldflags "-X main.Buildstamp=$buildstamp -X main.Hash=$hash  -X main.Version=$version" ${PROJECT_NAME}
+
+echo Compiling for Mac...
+export GOARCH=amd64
+export GOOS=darwin
+go build -o /usr/local/bin/${PROJECT_NAME}_mac -ldflags "-X main.Buildstamp=$buildstamp -X main.Hash=$hash  -X main.Version=$version" ${PROJECT_NAME}
