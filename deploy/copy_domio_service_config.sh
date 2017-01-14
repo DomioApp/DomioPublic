1c866428041b39cc533a6ec38bbf57097a159598
Sergei Basharov <sergey@basharov.net>
2017-01-14 15:45:57 +0200

    trying out golang templates

--- deploy/copy_domio_service_config.sh
+++ deploy/copy_domio_service_config.sh
@@ -6,5 +6,7 @@ echo Copying Domio Service Config...
 yes | cp -rf ~/domiopublic/deploy/config/domio_service.sh /etc/init.d/domio_public
 
 systemctl daemon-reload
+service domio_public stop
+service domio_public start
 
 echo Domio Service Config copied!
