70e998014f2e6ac19c1708853b79331f0711d0a5
Sergei Basharov <sergey@basharov.net>
2017-01-17 18:51:25 +0200

    added quicktemplate dependency

--- deploy/install_deps.sh
+++ deploy/install_deps.sh
@@ -8,5 +8,7 @@ go get -u -v github.com/fatih/color
 go get -u -v github.com/gorilla/mux
 go get -u -v github.com/gorilla/context
 go get -u -v github.com/yosssi/ace
+go get -u -v github.com/valyala/quicktemplate
+go get -u -v github.com/valyala/quicktemplate/qtc
 
 echo Go dependencies installed!
