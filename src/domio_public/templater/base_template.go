99165131b12738543a55b2f18244c9e528c2ced3
Sergey Basharov <sbasharov@spotware.com>
2017-02-08 17:53:44 +0200

    mobile viewport

--- src/domio_public/templater/base_template.go
+++ src/domio_public/templater/base_template.go
@@ -9,7 +9,7 @@ func getBaseTemplateContent() string {
 
                             <head>
                                 <meta charset="UTF-8">
-                                <meta name="viewport" content="width=device-width, initial-scale=1">
+                                <meta name="viewport" content="initial-scale=1, maximum-scale=1">
                                 <meta name="description" content="Domio is a marketplace for domains.">
                                 <meta name="page" content="{{.BaseTemplateData.PageName}}">
 
