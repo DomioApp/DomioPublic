1c3b605ab6e83ef2d1f484d5580a38e2b12ef059
Sergei Basharov <sergey@basharov.net>
2017-02-12 20:33:01 +0200

    commented api status info bar items

--- src/domio_public/templater/app_status_bar.go
+++ src/domio_public/templater/app_status_bar.go
@@ -4,12 +4,12 @@ func GetAppStatusInfoBarTemplate() string {
     appStatusInfoBarTemplate := `{{ define "app_status_infobar"}}
                                     {{with .AppStatusInfoBarData}}
                                         <div class="b-app-status-info">
-                                            <div><span>API Info</span></div>
+                                            <!--<div><span>API Info</span></div>-->
                                             <div>Version: <span>{{.Version}}</span></div>
                                             <div>Built: <span>{{.BuildAgo}} ago</span></div>
-                                            <div>Buildstamp: <span>{{.Buildstamp}}</span></div>
-                                            <div>BuildDateTime: <span>{{.BuildDateTime}}</span></div>
-                                            <div>Hash: <span>{{.Hash}}</span></div>
+                                            <!--<div>Buildstamp: <span>{{.Buildstamp}}</span></div>-->
+                                            <!--<div>BuildDateTime: <span>{{.BuildDateTime}}</span></div>-->
+                                            <!--<div>Hash: <span>{{.Hash}}</span></div>-->
                                         </div>
                                     {{end}}
                                  {{end}}
