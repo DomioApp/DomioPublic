e06057211c440f044d26aaf8ab02619e97f2fee4
Sergey Basharov <sbasharov@spotware.com>
2017-02-22 14:40:37 +0200

    added select for record type

--- src/domio_public/handlers/subscription_page_handler/subscription_page_config_main_view.go
+++ src/domio_public/handlers/subscription_page_handler/subscription_page_config_main_view.go
@@ -20,8 +20,23 @@ func GetSubscriptionConfigMainViewTemplate() string {
                         </div>
 
                         <form class="b-subscription-record-form">
-                            <input class="value-input"></input>
+                            <input class="subscription-id-input" type="hidden" value="{{.Subscription.Id}}"></input>
 
+                            <select class="value-type-select">
+                                <option value="a">A – IPv4 address</option>
+                                <option value="cname">CNAME – Canonical name</option>
+                                <option value="mx">MX – Mail exchange</option>
+                                <option value="aaaa">AAAA – IPv6 address</option>
+                                <option value="txt">TXT – Text</option>
+                                <option value="ptr">PTR – Pointer</option>
+                                <option value="srv">SRV – Service locator</option>
+                                <option value="spf">SPF – Sender Policy Framework</option>
+                                <option value="naptr">NAPTR – Name Authority Pointer</option>
+                                <option value="ns">NS – Name server</option>
+                                <option value="soa" disabled="disabled" title="Only one SOA record can exist in a zone, you can select it to edit its properties.">SOA – Start of authority</option>
+                            </select>
+
+                            <input class="value-input"></input>
                             <input type="submit" class="update-subscription-button" type="submit" value="Save"></input>
                         </form>
 
